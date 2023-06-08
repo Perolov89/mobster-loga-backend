c959df5cfad42954d9f2e89b5dc374662f05c0e2
Olle <olle.stalberg@appliedtechnology.se>
2023-06-08 11:29:02 +0200

    cors added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const mobsRouter = require('./routes/mobs'); // <-- added
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mobs', mobsRouter); // <-- added
